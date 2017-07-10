b04a2cc2b6b87522870d5d3c701d019d0cfd8796
AA Eftekhari <e.eftekhari@gmail.com>
2017-07-10 07:54:34 +0200

    changes to document

--- compression_exergy.jl
+++ compression_exergy.jl
@@ -4,7 +4,7 @@
 # for CO2, the maxiumum velocity is 50 ft/s
 # minimum gas velocity is 10 to 15 ft/s
 # %% source: http://petrowiki.org/Pipeline_design_consideration_and_standards#Gas_line_sizing
-using Plots, Roots, CoolProp, DataFrames
+using PyPlot, Roots, CoolProp, DataFrames
 # include("CoolProp.jl")
 
 # %% constants
@@ -35,12 +35,12 @@ T_res= 90+273.15 # K
 # gas_type="CO2"
 gas_type="N2"
 # a mixture can be defined as:
-#gas_type="CO2[0.7]&N2[0.3]"
+gas_type="CO2[0.9]&N2[0.1]"
 
 # pipe line specifications
 Q_g= 109260/(24*3600) # m^3/s at reservoir condition
 L_pipe=200000.0 # m
-n_segments=200
+n_segments=50
 T_pipe=35+273.15 # K pipe line temperature
 p_end=80e5 # pressure at the end of pipe line
 # p_sat_gas=CoolProp.PropsSI("P","T",T_pipe,"Q",0.0,gas_type)
@@ -66,29 +66,29 @@ d=(4*Q_end/(π*v_g))^0.5 # m pipe diameter
 d_inch=d*m_to_inch
 
 # p_sat_gas=CoolProp.PropsSI("P","T",T_pipe,"Q",0.0,gas_type)
-C_gas_res=CoolProp.PropsSI("DMOLAR", "T", T_res,
-  "P", p_res, gas_type) # gas density mol/m3
-Q_g_molar=Q_g*C_gas_res # mol/s molar flow rate
+# C_gas_res=CoolProp.PropsSI("DMOLAR", "T", T_res,
+#   "P", p_res, gas_type) # gas density mol/m3
+# Q_g_molar=Q_g*C_gas_res # mol/s molar flow rate
 
-C_gas_end=CoolProp.PropsSI("DMOLAR", "T", T_pipe,
-  "P", p_end, gas_type) # gas density mol/m3
-Q_end=Q_g_molar/C_gas_end # m^3/s flow at the end segment
+# C_gas_end=CoolProp.PropsSI("DMOLAR", "T", T_pipe,
+#   "P", p_end, gas_type) # gas density mol/m3
+# Q_end=Q_g_molar/C_gas_end # m^3/s flow at the end segment
 C_gas_std=CoolProp.PropsSI("DMOLAR", "T", T_ref,
   "P", p_ref, gas_type) # gas density mol/m3
 Q_g_std=Q_g_molar/C_gas_std
-d=(4*Q_end/(π*v_g))^0.5 # m pipe diameter
-d_inch=d*m_to_inch
+# d=(4*Q_end/(π*v_g))^0.5 # m pipe diameter
+# d_inch=d*m_to_inch
 e_pipe=0.9 # pipe efficiency
 # p_sat_gas=CoolProp.PropsSI("P","T",T_pipe,"Q",0.0,gas_type)
-C_gas_res=CoolProp.PropsSI("DMOLAR", "T", T_res,
-  "P", p_res, gas_type) # gas density mol/m3
-Q_g_molar=Q_g*C_gas_res # mol/s molar flow rate
+# C_gas_res=CoolProp.PropsSI("DMOLAR", "T", T_res,
+#   "P", p_res, gas_type) # gas density mol/m3
+# Q_g_molar=Q_g*C_gas_res # mol/s molar flow rate
 
-C_gas_end=CoolProp.PropsSI("DMOLAR", "T", T_pipe,
-  "P", p_end, gas_type) # gas density mol/m3
-Q_end=Q_g_molar/C_gas_end # m^3/s flow at the end segment
-d=(4*Q_end/(π*v_g))^0.5 # m pipe diameter
-d_inch=d*m_to_inch
+# C_gas_end=CoolProp.PropsSI("DMOLAR", "T", T_pipe,
+#   "P", p_end, gas_type) # gas density mol/m3
+# Q_end=Q_g_molar/C_gas_end # m^3/s flow at the end segment
+# d=(4*Q_end/(π*v_g))^0.5 # m pipe diameter
+# d_inch=d*m_to_inch
 d_inch_min=d_inch-d_inch%2 # make it a standard pipe diameter
 
 # start the calculations here
@@ -115,7 +115,7 @@ for j in 1:5
     p[i]=sqrt((p[i+1]*pa_to_psi)^2+sg_gas^0.961*Z_gas*L_pipe/n_segments*m_to_mile*T_pipe*K_to_degR
     *(Q_g_std*sm3s_to_mmscfd/(0.028*e_pipe*d_inch^2.53))^(1.0/0.51))/pa_to_psi
   end
-
+  info("pipeline pressure drop is calculated.")
   # plot(linspace(0, L_pipe, n_segments+1)/1000, p/1e5,
   #      xlabel="Pipe length [km]", ylabel="P [bar]")
 
@@ -147,7 +147,13 @@ for j in 1:5
     H_in=CoolProp.PropsSI("HMOLAR", "T", T_comp, "P", p_comp[i], gas_type) # molar enthalpy J/mol
     # find the output T for isentropic compressor
     #S_in-CoolProp.PropsSI("SMOLAR", "T", T_comp, "P", p_out, gas_type)
-    T_out=CoolProp.PropsSI("T", "SMOLAR", S_in, "P", p_comp[i+1], gas_type)
+    if gas_type == "CO2" || gas_type == "N2" || gas_type == "CH4"
+      T_out=CoolProp.PropsSI("T", "SMOLAR", S_in, "P", p_comp[i+1], gas_type)
+    else
+      ftemp = T -> S_in - CoolProp.PropsSI("SMOLAR", "T", T, "P", p_comp[i+1], gas_type)
+      T_out = fzero(ftemp, T_comp+40)
+    end
+    # H_out=CoolProp.PropsSI("HMOLAR", "SMOLAR", S_in, "P", p_comp[i+1], gas_type)
     H_out=CoolProp.PropsSI("HMOLAR", "T", T_out, "P", p_comp[i+1], gas_type) # molar enthalpy J/mol
     w_min_transport+=H_out-H_in # J/mol
   end
@@ -173,7 +179,13 @@ for j in 1:5
     H_in=CoolProp.PropsSI("HMOLAR", "T", T_comp, "P", p_comp_well[i], gas_type) # molar enthalpy J/mol
     # find the output T for isentropic compressor
     #S_in-CoolProp.PropsSI("SMOLAR", "T", T_comp, "P", p_out, gas_type)
-    T_out=CoolProp.PropsSI("T", "SMOLAR", S_in, "P", p_comp_well[i+1], gas_type)
+    if gas_type == "CO2" || gas_type == "N2" || gas_type == "CH4"
+      T_out=CoolProp.PropsSI("T", "SMOLAR", S_in, "P", p_comp_well[i+1], gas_type)
+    else
+      ftemp = T -> S_in - CoolProp.PropsSI("SMOLAR", "T", T, "P", p_comp_well[i+1], gas_type)
+      T_out = fzero(ftemp, T_comp+40)
+    end
+    # H_out=CoolProp.PropsSI("HMOLAR", "SMOLAR", S_in, "P", p_comp_well[i+1], gas_type)
     H_out=CoolProp.PropsSI("HMOLAR", "T", T_out, "P", p_comp_well[i+1], gas_type) # molar enthalpy J/mol
     w_min_well+=H_out-H_in # J/mol
   end
@@ -195,7 +207,7 @@ for j in 1:5
   push!(results_out["injection exergy th"], Q_g_molar*w_min_well)
 end
 d_results=DataFrame(results_out)
-writetable("result_$gas_type.csv", d_results)
+# writetable("result_$gas_type.csv", d_results)
 
 # writedlm("result_$gas_type.csv", results_out, ",")
 # rho_gas=CoolProp.PropsSI("DMOLAR", "T", temperature_in_K,
